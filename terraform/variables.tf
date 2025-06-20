5e7935a8e7feafe5f77a596cb4fcbd325369701f
addy-47 <adhbutg@gmail.com>
2025-06-20 11:36:25 +0530

    started tf config with provider & variables

---
+++ terraform/variables.tf
@@ -0,0 +1,28 @@
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "The GCP region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "db_username" {
+  description = "Database username"
+  type        = string
+  default     = "taskuser"
+}
+
+variable "db_password" {
+  description = "Database password"
+  type        = string
+  sensitive   = true
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "dev"
+}
